0a63c6972470c17c8a3daa89624edc6f4770cd47
Cong Zhang <njzc@live.com>
2015-03-27 16:45:53 -0400

    Add asking user to input a sentence

--- Assginment4/src/Recursion.java
+++ Assginment4/src/Recursion.java
@@ -1,10 +1,26 @@
+/*
+	1.Name: Cong Zhang 
+	2.ID number: 260573307
+	3.Course number: CCCS 301 
+	4.Assignment number: 4 
+	5.Where you developed your program: Eclipse
+*/
+
+import java.util.Scanner;
 
 public class Recursion {
 
 
 	public static void main(String[] args) {
 
-		System.out.println(ReversePrint("This is mary"));
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please input a single sentence: ");
+		String inputString = scanner.nextLine();
+		System.out.println("The reverse of this sentence is: ");
+		System.out.println(ReversePrint(inputString));
+		
+		scanner.close();
 	}
 
 	public static String ReversePrint(String msg)
